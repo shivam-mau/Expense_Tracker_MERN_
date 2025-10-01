f796f2e6d1613fcb40819668a994eb95e2ac7681
Shivam Maurya <shivam.maurya2024bca@gmail.com>
2025-10-01 10:55:54 +0530

    mostly completed backend work

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try{
 
         await mongoose.connect(process.env.MONGO_URI,{});
-        console.log("MongoBD connected");
+        console.log("MongoDB connected");
     } catch(err){
         console.error("Error connecting to MongoDB",err);
         process.exit(1);
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,15 @@ const jwt = require("jsonwebtoken");
 //Gentrate jwt token
 const generateToken =(id) =>
 {
-    return jwt.sign({id}.process.env.JWT_SECRET,{expiresIn: "1h"})
+    return jwt.sign({ id },process.env.JWT_SECRET, {expiresIn: "7d"})
 }
 
 //register user
 exports.registerUser = async(req, res) =>{
 
-    const {fullName, email, password, profileImageUrl} = req.body;
+
+    const body = req.body || {};
+    const{ fullName, email, password, profileImageUrl} = body;
     
 
     //validate check for missing fields
@@ -22,13 +24,13 @@ exports.registerUser = async(req, res) =>{
     }
     try{
         //check if email alreay exists
-        const existingUser = await User.find({email});
+        const existingUser = await User.findOne({email});
         if(existingUser){
             return res.status(400).json({message: "Email already in use"});
         }
 
         //create the user
-        const User = await User.create({
+        const user = await User.create({
             fullName,
             email,
             password,
@@ -48,7 +50,42 @@ exports.registerUser = async(req, res) =>{
 };
 
 //login user
-exports.loginUser = async(req, res) =>{};
+exports.loginUser = async(req, res) =>{
+    const{ email, password}= req.body;
+   if(!email || !password){
+    return res.status(400).json({messagw: "All fields are required"});
+   }
+   try{
+    const user = await User.findOne({ email });
+    if(!user || !(await user.comparePassword(password))){
+        return res.status(400).json({message: "Invalid credentials"});
+    }
+    res.status(200).json({
+        id: user._id,
+        user,
+        token: generateToken(user._id),
+    });
+   } catch(err){
+     res
+            .status(500)
+            .json({message: "Error registering user", error: err.message});
+   }
+};
 
 //get user information
-exports.getUserInfo = async(req, res) =>{};
+exports.getUserInfo = async(req, res) =>{
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+    
+    if (!user) {
+        return res.status(400).json({message: "User not found"});
+    }
+                                                       
+
+    res.status(200).json(user);
+    } catch (err) {
+         res
+            .status(500)
+            .json({message: "Error registering user", error: err.message});
+    }
+};
